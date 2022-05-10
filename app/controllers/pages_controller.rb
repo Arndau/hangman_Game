22857f190822296e2bb67ecbad766dbd04b68ff5
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-10 14:56:02 +0200

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-  def home
+  def partie
+    partie = Game.New
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,33 @@
 class Game < ApplicationRecord
+
+  def initialize
+    @letters = ("a".."z").to_a
+    @word = words.sample
+  end
+
+  def words
+    words =
+    [
+      "spokesperson", "firefighter", "headquarters", "confession", "difficulty", "attachment", "mechanical",
+      "accumulation", "hypothesis", "systematic", "attraction", "distribute", "dependence", "environment",
+      "jurisdiction", "demonstrator", "constitution", "constraint", "consumption", "presidency", "incredible",
+      "miscarriage", "foundation", "photography", "constituency", "experienced", "background", "obligation",
+      "diplomatic", "discrimination", "entertainment", "grandmother", "girlfriend", "conversation", "convulsion",
+      "constellation", "leadership", "insistence", "projection", "transparent", "researcher", "reasonable", "continental",
+      "excavation", "opposition", "interactive", "pedestrian", "announcement", "charismatic", "strikebreaker",
+      "resolution", "professional", "commemorate", "disability", "collection", "cooperation", "embarrassment",
+      "contradiction", "unpleasant", "retirement", "conscience", "satisfaction", "acquaintance", "expression",
+      "difference", "unfortunate", "accountant", "information", "fastidious", "conglomerate", "shareholder",
+      "accessible", "advertising", "battlefield", "laboratory", "manufacturer", "acquisition", "operational",
+      "expenditure", "fashionable", "allocation", "complication", "censorship", "population", "withdrawal",
+      "sensitivity", "exaggerate", "transmission", "philosophy", "memorandum", "superintendent", "responsibility",
+      "extraterrestrial", "hypothesize", "ghostwriter", "representative", "rehabilitation", "disappointment",
+      "understanding", "supplementary", "preoccupation"
+    ]
+  end
+
+  def begin_game
+    puts "New game started"
+  end
+
 end

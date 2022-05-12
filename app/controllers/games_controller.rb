8e436743df724ba5d4d1a98b0d7d31b607fe3f5c
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-12 16:22:15 +0200

    seed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,44 +13,6 @@ class GamesController < ApplicationController
               # - if bad_choices == 1 alors afficher la deuxieme image,
               # - ETC ...
 
-    letters = ("a".."z").to_a
-    words =
-    [
-      "spokesperson", "firefighter", "headquarters", "confession", "difficulty", "attachment", "mechanical",
-      "accumulation", "hypothesis", "systematic", "attraction", "distribute", "dependence", "environment",
-      "jurisdiction", "demonstrator", "constitution", "constraint", "consumption", "presidency", "incredible",
-      "miscarriage", "foundation", "photography", "constituency", "experienced", "background", "obligation",
-      "diplomatic", "discrimination", "entertainment", "grandmother", "girlfriend", "conversation", "convulsion",
-      "constellation", "leadership", "insistence", "projection", "transparent", "researcher", "reasonable", "continental",
-      "excavation", "opposition", "interactive", "pedestrian", "announcement", "charismatic", "strikebreaker",
-      "resolution", "professional", "commemorate", "disability", "collection", "cooperation", "embarrassment",
-      "contradiction", "unpleasant", "retirement", "conscience", "satisfaction", "acquaintance", "expression",
-      "difference", "unfortunate", "accountant", "information", "fastidious", "conglomerate", "shareholder",
-      "accessible", "advertising", "battlefield", "laboratory", "manufacturer", "acquisition", "operational",
-      "expenditure", "fashionable", "allocation", "complication", "censorship", "population", "withdrawal",
-      "sensitivity", "exaggerate", "transmission", "philosophy", "memorandum", "superintendent", "responsibility",
-      "extraterrestrial", "hypothesize", "ghostwriter", "representative", "rehabilitation", "disappointment",
-      "understanding", "supplementary", "preoccupation"
-    ]
-    images = ["1/5","2/5","3/5","4/5","5/5","6/5"]
-    user_choice =
-    def initialize
-      @letters = letters
-      @word = words.sample
-      @image = images.first
-      @user_choice = user_choice
-      @choices << user_choice
-    end
-
-       # TO DO : télécharger les images sur cloudinary et connecter à cloudinary
-  def images
-    ["1/5","2/5","3/5","4/5","5/5","6/5"]
-  end
-
-
-  def partie
-
-  end
 
 
 end

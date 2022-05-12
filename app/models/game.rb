7d2d75d4ddab8b489ff170e53e2cb45520f24796
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-12 16:34:10 +0200

    creat migrations for model game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,24 +1,4 @@
 class Game < ApplicationRecord
 
-  def words
-    words =
-    [
-      "spokesperson", "firefighter", "headquarters", "confession", "difficulty", "attachment", "mechanical",
-      "accumulation", "hypothesis", "systematic", "attraction", "distribute", "dependence", "environment",
-      "jurisdiction", "demonstrator", "constitution", "constraint", "consumption", "presidency", "incredible",
-      "miscarriage", "foundation", "photography", "constituency", "experienced", "background", "obligation",
-      "diplomatic", "discrimination", "entertainment", "grandmother", "girlfriend", "conversation", "convulsion",
-      "constellation", "leadership", "insistence", "projection", "transparent", "researcher", "reasonable", "continental",
-      "excavation", "opposition", "interactive", "pedestrian", "announcement", "charismatic", "strikebreaker",
-      "resolution", "professional", "commemorate", "disability", "collection", "cooperation", "embarrassment",
-      "contradiction", "unpleasant", "retirement", "conscience", "satisfaction", "acquaintance", "expression",
-      "difference", "unfortunate", "accountant", "information", "fastidious", "conglomerate", "shareholder",
-      "accessible", "advertising", "battlefield", "laboratory", "manufacturer", "acquisition", "operational",
-      "expenditure", "fashionable", "allocation", "complication", "censorship", "population", "withdrawal",
-      "sensitivity", "exaggerate", "transmission", "philosophy", "memorandum", "superintendent", "responsibility",
-      "extraterrestrial", "hypothesize", "ghostwriter", "representative", "rehabilitation", "disappointment",
-      "understanding", "supplementary", "preoccupation"
-    ]
-  end
 
 end
--- db/migrate/20220510111733_create_games.rb
+++ db/migrate/20220510111733_create_games.rb
@@ -1,7 +1,8 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
-
+      t.string :word_to_guess
+      t.integer :health_bar
       t.timestamps
     end
   end

53fc57900d9989df1e7e48a1c7d2b6c075c287a6
Le Garrec Arnaud <legarrecarnaud.92@gmail.com>
2022-05-16 13:39:24 +0200

    plenty of modifications, seed removed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,8 +19,14 @@ class GamesController < ApplicationController
   end
 
   def new
-    Game.destroy_all
+    # Game.destroy_all
     @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save!
+    redirect_to game_path(@game)
   end
 
   def show
@@ -28,14 +34,40 @@ class GamesController < ApplicationController
     @game = Game.last
   end
 
-  def word_to_guess
-    @word_to_guess = words.shuffle
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    # redirect_to games_path
 
   end
   private
 
-  def politic_params
-    params.require(:game).permit(:word_to_guess, :health_bar, :game_status)
+  words =[
+    "spokesperson", "firefighter", "headquarters", "confession", "difficulty", "attachment", "mechanical",
+    "accumulation", "hypothesis", "systematic", "attraction", "distribute", "dependence", "environment",
+    "jurisdiction", "demonstrator", "constitution", "constraint", "consumption", "presidency", "incredible",
+    "miscarriage", "foundation", "photography", "constituency", "experienced", "background", "obligation",
+    "diplomatic", "discrimination", "entertainment", "grandmother", "girlfriend", "conversation", "convulsion",
+    "constellation", "leadership", "insistence", "projection", "transparent", "researcher", "reasonable","continental",
+    "excavation", "opposition", "interactive", "pedestrian", "announcement", "charismatic", "strikebreaker",
+    "resolution", "professional", "commemorate", "disability", "collection", "cooperation", "embarrassment",
+    "contradiction", "unpleasant", "retirement", "conscience", "satisfaction", "acquaintance", "expression",
+    "difference", "unfortunate", "accountant", "information", "fastidious", "conglomerate", "shareholder",
+    "accessible", "advertising", "battlefield", "laboratory", "manufacturer", "acquisition", "operational",
+    "expenditure", "fashionable", "allocation", "complication", "censorship", "population", "withdrawal",
+    "sensitivity", "exaggerate", "transmission", "philosophy", "memorandum", "superintendent", "responsibility",
+    "extraterrestrial", "hypothesize", "ghostwriter", "representative", "rehabilitation", "disappointment",
+    "understanding", "supplementary", "preoccupation"
+ ]
+
+images = ["1/5","2/5","3/5","4/5","5/5","6/5"]
+
+  @word_to_guess = words.sample
+  @health_bar = 5
+  @game_status = "Game not started yet"
+
+  def game_params
+    params.require(:game).permit(@word_to_guess, @health_bar, @game_status)
   end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,26 @@ class Game < ApplicationRecord
   has_many :guesses
   validates :game_status, inclusion: {in: ["Game not started yet", "In Game", "You win", "You loose"] }
 
+  words =[
+    "spokesperson", "firefighter", "headquarters", "confession", "difficulty", "attachment", "mechanical",
+    "accumulation", "hypothesis", "systematic", "attraction", "distribute", "dependence", "environment",
+    "jurisdiction", "demonstrator", "constitution", "constraint", "consumption", "presidency", "incredible",
+    "miscarriage", "foundation", "photography", "constituency", "experienced", "background", "obligation",
+    "diplomatic", "discrimination", "entertainment", "grandmother", "girlfriend", "conversation", "convulsion",
+    "constellation", "leadership", "insistence", "projection", "transparent", "researcher", "reasonable","continental",
+    "excavation", "opposition", "interactive", "pedestrian", "announcement", "charismatic", "strikebreaker",
+    "resolution", "professional", "commemorate", "disability", "collection", "cooperation", "embarrassment",
+    "contradiction", "unpleasant", "retirement", "conscience", "satisfaction", "acquaintance", "expression",
+    "difference", "unfortunate", "accountant", "information", "fastidious", "conglomerate", "shareholder",
+    "accessible", "advertising", "battlefield", "laboratory", "manufacturer", "acquisition", "operational",
+    "expenditure", "fashionable", "allocation", "complication", "censorship", "population", "withdrawal",
+    "sensitivity", "exaggerate", "transmission", "philosophy", "memorandum", "superintendent", "responsibility",
+    "extraterrestrial", "hypothesize", "ghostwriter", "representative", "rehabilitation", "disappointment",
+    "understanding", "supplementary", "preoccupation"
+ ]
+
+images = ["1/5","2/5","3/5","4/5","5/5","6/5"]
+
+@word_to_guess = words.sample
+
 end
